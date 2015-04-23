ea7dd21b528e49a5eda028d73efc8748953b5e5e
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-04-23 15:26:21 +0100

    Initiated ACF options page for header image switch

--- wp-content/themes/linen_pro/functions.php
+++ wp-content/themes/linen_pro/functions.php
@@ -1,2 +1,8 @@
 <?php
 	locate_template( array( 'functions' . DIRECTORY_SEPARATOR . 'linen-extend.php' ), true );
+	
+		if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page();
+	
+}
